e005a52bef6163fecd8b76680b8a50abfcdc452c
Tomas Meleg <tomas.meleg@gmail.com>
2024-06-17 23:33:25 +0200

    change live url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ const SERVER_PORT = 3000;
 // url for local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // url for live website
-const LIVE_URL = `https://m311ow.github.io`;
+const LIVE_URL = `https://epdclarity.com`;
 // this is the astro command that npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || '';
 const isBuild = SCRIPT.includes('astro build');
