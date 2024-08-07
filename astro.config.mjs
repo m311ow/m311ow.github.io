735ab981d0af10e17888f9d1adc14c58cee6a471
Tomas Meleg <tomas.meleg@gmail.com>
2024-08-07 09:31:00 +0200

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import purgecss from 'astro-purgecss';
 
 // localhost server port
+import partytown from '@astrojs/partytown';
 const SERVER_PORT = 3000;
 // url for local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
@@ -11,7 +12,6 @@ const LIVE_URL = `https://epdclarity.com`;
 // this is the astro command that npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || '';
 const isBuild = SCRIPT.includes('astro build');
-
 let BASE_URL = LOCALHOST_URL;
 if (isBuild) {
   BASE_URL = LIVE_URL;
@@ -19,7 +19,9 @@ if (isBuild) {
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
+  server: {
+    port: SERVER_PORT
+  },
   site: BASE_URL,
   integrations: [
     sitemap(),
@@ -27,6 +29,12 @@ export default defineConfig({
       standard: ['html', 'body'],
       safelist: {
         greedy: [/swiper$/, /^swiper/]
+      }
+    }),
+    partytown({
+      config: {
+        // for Partytown to know to forward any dataLayer.push events to the worker
+        forward: ['dataLayer.push']
       }
     })
   ],
