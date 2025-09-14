12e807c5c55c2a35ae36467c197b98ce03180b81
Tomas Meleg <tomas.meleg@gmail.com>
2025-09-14 11:51:40 +0200

    disable dart sass deprecation warning in terminal

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,15 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salvis.eu',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  // get rid of Dart Sass deprecation warning
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
+  }
 })
