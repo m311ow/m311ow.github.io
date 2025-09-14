eb3305bb9bb21fdb52d324b517e09c49796749c1
Tomas Meleg <tomas.meleg@gmail.com>
2025-09-14 11:49:51 +0200

    add sitemap integration to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://m311ow.github.io'
-});
+  site: 'https://salvis.eu',
+  integrations: [sitemap()]
+})
